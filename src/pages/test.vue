05801fae43dff9fba7230ba8cf8ad620eb962344
李思思 <lisisi@lisisideMacBook-Pro.local>
2018-10-17 16:10:41 +0800

    [NEW] ADD TEST

---
+++ src/pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+ <div id=''>
+
+ </div>
+</template>
+
+<script>
+export default {
+ name: '',
+ data () {
+ return {
+ }
+ },
+ computed: {
+   log
+ },
+ created: {},
+ mounted: {},
+ methods: {}
+}
+</script>
+
+<style lang='less' scoped>``
+# {}
+</style>
